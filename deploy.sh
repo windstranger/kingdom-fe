cd38a47e5dcbf31df729ee3365f87a8820b58969
“akondratov” <akondratov@it-one.ru>
2024-04-07 20:56:17 +0300

    add unity calls

---
+++ deploy.sh
@@ -0,0 +1,1 @@
+rsync -avh dist twin@element5.pro:/home/twin/drochers-fe-dist
--- src/UnityContainer.jsx
+++ src/UnityContainer.jsx
@@ -1,10 +1,11 @@
 import React, {useCallback, useEffect, useState} from "react";
-import { Unity, useUnityContext, UnityEvent } from "react-unity-webgl";
+import {Unity, useUnityContext, UnityEvent} from "react-unity-webgl";
+
 export function UnityContainer() {
 
     const [score, setScore] = useState(0);
 
-    const { unityProvider, addEventListener, removeEventListener, sendMessage } = useUnityContext({
+    const {unityProvider, addEventListener, removeEventListener, sendMessage} = useUnityContext({
         loaderUrl: "/build/out.loader.js",
         dataUrl: "/build/out.data.gz",
         frameworkUrl: "/build/out.framework.js.gz",
@@ -12,7 +13,7 @@ export function UnityContainer() {
     });
 
 
-    const handleScoreUpdate = useCallback((score)=>{
+    const handleScoreUpdate = useCallback((score) => {
         setScore(score);
     });
 
@@ -23,11 +24,63 @@ export function UnityContainer() {
         };
     }, [addEventListener, removeEventListener, handleScoreUpdate]);
 
+    const roundStart = () => {
+        const gameParameters = {
+            playerCards: [
+                1, 2, 3
+            ],
+            world: {
+                a2: {
+                    card: [1, 2],
+                    player: 2
+                }
+            }
+        }
+        sendMessage("GameController", "roundStart", JSON.stringify(gameParameters));
+    }
+    const gameEnd = () => {
+        const gameParameters = {
+            playerScore: 123
+        }
+        sendMessage("GameController", "roundStart", JSON.stringify(gameParameters));
+    }
+    const gameEndToPlayers = () => {
+        const gameParameters =
+            [
+                {
+                    playerId: 123,
+                    playerScore: 123
+                }
+            ]
+
+
+        sendMessage("GameController", "gameEndToPlayers", JSON.stringify(gameParameters));
+    }
+
+    const playerTurnEnd = () => {
+        const gameParameters = {
+            // оставшиеся карточки для другого игрока
+            playerCards: [
+                1, 2, 3
+            ],
+            // сделанный ход игроком
+            world: {
+                a2: {
+                    card: [1, 2],
+                    player: 2
+                }
+            }
+        }
+        // вызов из юнити реакт
+        // sendMessage("GameController", "roundStart", JSON.stringify(gameParameters));
+    }
+
+
     const callUnity = useCallback(() => {
         sendMessage("GameController", "SpawnEnemies", 100);
     }, [sendMessage]);
     return <div>
         <button onClick={callUnity}>call unity</button>
-        <Unity unityProvider={unityProvider} style={{width: "100vw", height: "100vh", overflow: "hidden", zIndex: 0}} />;
+        <Unity unityProvider={unityProvider} style={{width: "100vw", height: "100vh", overflow: "hidden", zIndex: 0}}/>;
     </div>
 }
